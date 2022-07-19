a85adf312dcd9171eead5e7a72473c45bd8de170
Priyamvada <50589695+Priyamvada03@users.noreply.github.com>
2022-07-19 21:42:56 +0530

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports={
+    MONGOURI:"mongodb+srv://priyamvada:Priyamvada@1999@cluster0.nbce4.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET:"gdvfthhj",
+SENDGRID_API:"XYZ",
+EMAIL:"http:localhost:3000"
+}
